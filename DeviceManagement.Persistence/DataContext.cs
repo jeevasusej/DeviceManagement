b346184b3f6247e36e81ba251bb89e3387a4d728
Jeevanandan Jothinathan <jeevasusej@gmail.com>
2018-11-16 04:00:22 +0530

    message

---
+++ DeviceManagement.Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DeviceManagement.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
